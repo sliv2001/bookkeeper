a24075c41ddf8a47a69f0818b54d10b0c134496d
Ivan Sladkov <ivan.sladkov@yandex.ru>
2024-03-16 21:16:40 +0300

    [dev_step3] Stabilized category

--- bookkeeper/presenter/presenter.py
+++ bookkeeper/presenter/presenter.py
@@ -1,4 +1,5 @@
 from pony import orm
+from PySide6.QtCore import QObject, Signal
 
 from bookkeeper.repository.budget_repository import BudgetRepository
 from bookkeeper.repository.category_repository import CategoryRepository
@@ -7,16 +8,110 @@ from bookkeeper.models.budget import Budget
 from bookkeeper.models.category import Category
 from bookkeeper.models.expense import Expense
 
-class Presenter:
-    budgetModel: BudgetRepository
-    categoryModel: CategoryRepository
-    expenseModel: ExpenseRepository
+class Presenter(QObject):
+    _budgetRepo: BudgetRepository
+    _categoryRepo: CategoryRepository
+    _expenseRepo: ExpenseRepository
+
+    updated : Signal = Signal(bool)
+
+    _pendingCategoryChanges = []
 
     def __init__(self) -> None:
-        self.budgetModel = BudgetRepository('data.db3')
-        self.categoryModel = CategoryRepository('data.db3')
-        self.expenseModel = ExpenseRepository('data.db3')
+        super().__init__()
+        self._budgetRepo = BudgetRepository('data.db3')
+        self._categoryRepo = CategoryRepository('data.db3')
+        self._expenseRepo = ExpenseRepository('data.db3')
 
-    def addCategory(self, cat: str):
+    def _getCategoryChildren(self, item):
+        # Recursively traverse database and append res, which is dictionary {parent: [children]}
+        res = {}
+        parentCat = self._categoryRepo.getByName(item)
+        children = [x.name for x in self._categoryRepo.get_all(lambda x: x.parent == parentCat.pk)]
+        for child in children:
+            next_children = self._getCategoryChildren(child)
+            if len(next_children) != 0:
+                #TODO extract function from following:
+                res = dict(list(res.items()) + list(next_children.items()))
+                #TODO extract function from following:
+        return dict(list({item: children}.items()) + list(res.items()))
+
+# TODO replace cat[0], ... with class change
+
+    def _removeCategory(self, cat, topLevels, res):
+        # Remove category
+        if cat[0] in topLevels: topLevels.remove(cat[0])
+        # Remember entry, for which the removed list is a parent
+        # and rebind to parent of removed list
+        for key, lst in res:
+            if cat[0] in lst:
+                lst.remove(cat[0])
+                parent = key
+        for key in res.keys():
+            if key == cat[0]:
+                res[parent] += res[key]
+                del res[key]
+                
+    def getCategoriesHierarchy(self):
+        with orm.db_session():
+            topLevels = [x.name for x in self._categoryRepo.get_all(lambda x: x.parent == None)]
+            res = {}
+            for item in topLevels:
+                #TODO extract function from following:
+                res = dict(list(res.items()) + list(self._getCategoryChildren(item).items()))
+
+        # After collection we need to apply changes, if any
+        for cat in self._pendingCategoryChanges:
+            if cat[0] == None:
+                # Add new category
+                if cat[2]!=None:
+                    res[cat[2]]+=[cat[1]]
+                else:               
+                    topLevels+={cat[1]}
+                res[cat[1]]=[]
+            elif cat[1] == None:
+                self._removeCategory(cat, topLevels, res)
+            else:
+                # Rename category
+                if cat[0] in topLevels: topLevels.remove(cat[0])
+                for lst in res.items():
+                    if cat[0] in lst: lst.remove(cat[0])
+        return topLevels, res
+    
+    def getAllCategories(self):
+        return self.getCategoriesHierarchy()[1].keys()
+
+    def addCategory(self, cat: str, parent = None):
+        if self._categoryRepo.getByName(cat) != None:
+            raise RuntimeError('Entry already exists!')
+        self._pendingCategoryChanges.append([None, cat, parent])
+        self.updated.emit(True)
+
+    def commitCategories(self):
         with orm.db_session:
-            self.categoryModel.add(Category(name=cat))
+            for cat in self._pendingCategoryChanges:
+                if cat[0] == None:
+                    # Add new category
+                    if cat[2]==None:
+                        self._categoryRepo.add(Category(name=cat[1]))    
+                    else:
+                        parentCatPk=self._categoryRepo.getByName(cat[2]).pk
+                        self._categoryRepo.add(Category(name=cat[1], parent=parentCatPk))
+                elif cat[1] == None:
+                    # Remove category: rebind children to elder perent
+                    currentCat = Category(name=cat[0])
+                    for child in self._categoryRepo.get_all(lambda x: x.parent==currentCat.pk):
+                        child.parent = currentCat.parent
+                        self._categoryRepo.update(child)
+                    self._categoryRepo.delete()
+                else:
+                    # Update category
+                    # TODO check for wrong naming
+                    c = Category(name=cat[0])
+                    c.name = cat[1]
+                    c.parent = Category(name=cat[2]).pk
+                    self._categoryRepo.update(c)
+        self._pendingCategoryChanges = []
+    
+    def cancelCategories(self):
+        self._pendingCategoryChanges = []
--- bookkeeper/view/Category.py
+++ bookkeeper/view/Category.py
@@ -1,4 +1,4 @@
-from PySide6.QtCore import Qt, Slot
+from PySide6.QtCore import Qt, Slot, Signal
 from PySide6.QtWidgets import QDialog, QWidget, QMessageBox, QInputDialog, QTreeWidgetItem
 
 from bookkeeper.view.Ui_Category import Ui_CategoryDialog
@@ -7,6 +7,23 @@ from bookkeeper.presenter.presenter import Presenter
 class CategoryDialog(QDialog):
 
     presenter: Presenter
+    topLevels: list
+    dictOfAll: dict
+    
+    def displayCategories(self, parentWidget : QTreeWidgetItem = None):
+        if parentWidget == None:
+            childrenList = self.topLevels
+        else:
+            childrenList = self.dictOfAll[parentWidget.text(0)]
+            if childrenList==[]: 
+                return
+        for item in childrenList:
+            if parentWidget==None:
+                treeItem = QTreeWidgetItem(self.ui.treeWidget)
+            else:
+                treeItem = QTreeWidgetItem(parentWidget)
+            treeItem.setText(0, item)
+            self.displayCategories(treeItem)
 
     def __init__(self, presenter: Presenter = None, parent: QWidget | None = ..., flags: Qt.WindowType = ...) -> None:
         super(CategoryDialog, self).__init__()
@@ -16,21 +33,36 @@ class CategoryDialog(QDialog):
             self.presenter = presenter
         self.ui = Ui_CategoryDialog()
         self.ui.setupUi(self)
+        self.presenter.updated.connect(self.updateTreeWidget)
+        self.updateTreeWidget()
         
 
     @Slot()
     def on_pushButton_clicked(self):
-        item, res = QInputDialog.getText(self, 'Add category', 'New category: ')
-        newItem = QTreeWidgetItem(self.ui.treeWidget.)
+        newCat, res = QInputDialog.getText(self, 'New category', 'Name new category: ')
+        if res:
+            if len(self.dictOfAll.keys())>0:
+                parentCat, res = QInputDialog.getItem(self, 'Parent category', 'Choose parent category', list(self.dictOfAll.keys()) + ['None'])
+                if res:
+                    if parentCat=='None':
+                        self.presenter.addCategory(newCat)
+                    else:
+                        self.presenter.addCategory(newCat, parentCat)
+            else:
+                self.presenter.addCategory(newCat)
 
     @Slot()
     def accept(self) -> None:
-        for i in self.ui.treeWidget.columnCount():
-            for j in self.ui.treeWidget.columnAt():
-        # for i in range(self.ui.listWidget.count()):
-                self.presenter.addCategory(self.ui.treeWidget.itemAt(i, j).text())
+        self.presenter.commitCategories()
         return super().accept()
 
     @Slot()
     def reject(self) -> None:
+        self.presenter.cancelCategories()
         return super().reject()
+
+    @Slot()
+    def updateTreeWidget(self) -> None:
+        self.ui.treeWidget.clear()
+        self.topLevels, self.dictOfAll = self.presenter.getCategoriesHierarchy()
+        self.displayCategories()
